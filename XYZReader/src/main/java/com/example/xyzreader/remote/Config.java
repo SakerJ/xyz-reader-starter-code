18c5b210d1f80841ec6d8bd5530e6f3f7ecb7670
Saker Jing <306118788@qq.com>
2017-08-10 23:15:38 +0800

    Review third changes

--- XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
+++ XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
@@ -7,7 +7,8 @@ import java.net.URL;
 
 public class Config {
     //    public static final String API = "https://go.udacity.com/xyz-reader-json";
-    public static final String API = "https://nspf.github.io/XYZReader/data.json";
+//    public static final String API = "https://nspf.github.io/XYZReader/data.json";
+    public static final String API = "https://raw.githubusercontent.com/TNTest/xyzreader/master/data.json";
     public static final URL BASE_URL;
     private static String TAG = Config.class.toString();
 
